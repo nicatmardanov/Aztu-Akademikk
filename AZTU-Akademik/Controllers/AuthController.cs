80904c3ba9e7e8fd341e8e48820d13ca7cbace8e
Nicat Mardanov <nicaattttt@hotmail.com>
2020-09-17 13:16:29 +0400

    17/09/2020

---
+++ AZTU-Akademik/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AZTU_Akademik.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+
+        [HttpGet]
+        public JsonResult Get() => User.Identity.IsAuthenticated ? Json(new { isAuth = true, role = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role), id = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier) }) : Json(false);
+
+    }
+}
