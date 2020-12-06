3cabfa185bde758851904c700d33ec54171ec09f
Nicat Mardanov <nicaattttt@hotmail.com>
2020-12-06 23:18:37 +0400

    ,./

---
+++ AZTU-Akademik/Controllers/PositionController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AZTU_Akademik.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AZTU_Akademik.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PositionController : Controller
+    {
+        readonly private AztuAkademikContext aztuAkademik = new AztuAkademikContext();
+        private DateTime GetDate
+        {
+            get
+            {
+                return DateTime.UtcNow.AddHours(4);
+            }
+        }
+        private int User_Id
+        {
+            get
+            {
+                return int.Parse(User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value);
+            }
+        }
+
+
+        //GET
+        [HttpGet("Position")]
+        public JsonResult Position(int id) => Json(aztuAkademik.Position.FirstOrDefault(x => x.Id == id && !x.DeleteDate.HasValue));
+
+        [HttpGet("AllPositions")]
+        public JsonResult AllPositions() => Json(aztuAkademik.Position.Where(x => !x.DeleteDate.HasValue));
+
+
+        //POST
+        [HttpPost]
+        public async Task Post(Position _position)
+        {
+            _position.CreateDate = GetDate;
+
+            await aztuAkademik.Position.AddAsync(_position);
+            await aztuAkademik.SaveChangesAsync();
+        }
+
+        //PUT
+        [HttpPut]
+        public async Task<int> Put(Position _position)
+        {
+            if (ModelState.IsValid)
+            {
+                _position.UpdateDate = GetDate;
+                aztuAkademik.Attach(_position);
+                aztuAkademik.Entry(_position).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                aztuAkademik.Entry(_position).Property(x => x.CreateDate).IsModified = false;
+
+                await aztuAkademik.SaveChangesAsync();
+
+                return 1;
+            }
+            return 0;
+        }
+
+        //DELETE
+        [HttpDelete]
+        public async Task Delete(int id)
+        {
+            aztuAkademik.Position.FirstOrDefault(x => x.Id == id).DeleteDate = GetDate;
+            aztuAkademik.Position.FirstOrDefault(x => x.Id == id).StatusId = 0;
+
+            await aztuAkademik.SaveChangesAsync();
+        }
+
+    }
+}
